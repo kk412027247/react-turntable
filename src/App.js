cea1f4031eb6f8ac5cbef32a22eeadef1cd247f6
TMD <kk412027247@gmail.com>
2019-03-12 23:11:15 +0800

    setup turntable animation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  state={
+    rotate:'202.5',
+    number:0,
+    list:[],
+    enable:true,
+  };
+  // 八个格子，分别对应
+
+  random = () => Math.floor(Math.random() * 8);
+
+
+  handleClick = () => {
+    const {enable, number, rotate, list} =this.state;
+    if(!enable){
+      return;
+    }
+    const randomNumber =  this.random();
+    const deg = randomNumber > number ? randomNumber - number  : 8 - number + randomNumber ;
+    this.setState({
+      rotate: Number(rotate) + 720 + deg * 45  + '',
+      number:  '...',
+      enable: false,
+    });
+    this.timmer = setTimeout(()=>{this.setState({
+      number: randomNumber,
+      list:[...list, {item:randomNumber, time: Date.now()+''}],
+      enable: true,
+    })},1100)
+  };
+
+  componentWillUnmount(){
+    !!this.timmer && clearTimeout(this.timmer)
+  }
+
   render() {
+    const startStyle = {transform:`rotate(${this.state.rotate}deg)`};
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className={'turntable'}>
+          <img
+            src={require('./images/start.png')}
+            alt="pointer"
+            className={'start'}
+            style={startStyle}
+            onClick={this.handleClick}
+          />
+        </div>
+        <div className={'result'}>
+          {this.state.number}
+        </div>
+        <div className={'list'}>
+          {
+            this.state.list.slice().reverse().map(({item, time})=> <p key={time}>{item}</p>)
+          }
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
